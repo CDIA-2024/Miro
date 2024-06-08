152642fe0a42382e9587b9b2165e7a145b76d4c3
Cesia <cesiaf.gimenez@gmail.com>
2024-06-08 02:27:09 -0300

    Corrección de código y añadido de las tablas anime, videojuego y desarrollador

--- Diseño-base-de-datos/miru_bd.sql
+++ Diseño-base-de-datos/miru_bd.sql
@@ -1,47 +1,12 @@
-CREATE DATABASE  IF NOT EXISTS `miru` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci */;
-USE `miru`;
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: miru
--- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+CREATE DATABASE  IF NOT EXISTS `miru_1.1.1`;
+USE `miru_1.1.1`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `registro`
---
-
-DROP TABLE IF EXISTS `registro`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `registro` (
-  `id_resgistro` int(11) NOT NULL AUTO_INCREMENT,
-  `puntuacion` int(11) NOT NULL,
-  `estado` varchar(45) NOT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_resgistro`),
-  KEY `id_usuario` (`id_usuario`),
-  CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `usuario`
 --
 
 DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
@@ -52,10 +17,25 @@ CREATE TABLE `usuario` (
   `genero_de_interes` varchar(45) NOT NULL,
   PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `registro`
+--
+
+DROP TABLE IF EXISTS `registro`;
+CREATE TABLE `registro` (
+  `id_registro` int(11) NOT NULL AUTO_INCREMENT,
+  `puntuacion` int(11) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_registro`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
 -- Table structure for table 'lista'
+--
 
 DROP TABLE IF EXISTS `lista`;
 CREATE TABLE `lista` (
@@ -63,52 +43,93 @@ CREATE TABLE `lista` (
   `fecha_creacion` DATE NOT NULL,
   `id_usuario` INT(11) NOT NULL,
   PRIMARY KEY (`id_lista`),
-  FOREIGN KEY (`id_usuario`) REFERENCES `USUARIO` (`id_usuario`) ON UPDATE CASCADE ON DELETE CASCADE
+  FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
---Table structure for table 'contenido'
+--
+-- Table structure for table 'contenido'
+--
 
 DROP TABLE IF EXISTS `contenido`;
 CREATE TABLE `contenido` (
   `id_contenido` INT(11) NOT NULL AUTO_INCREMENT,
   `titulo` VARCHAR(255) NOT NULL,
-  `año_lanzamiento` YEAR NOT NULL,
+  `anio_lanzamiento` YEAR NOT NULL,
   PRIMARY KEY (`id_contenido`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
---Table structure for table 'Director'
+--
+-- Table strucre for table 'anime'
+--
 
-DROP TABLE IF EXISTS Director;
-CREATE TABLE Director (
+DROP TABLE IF EXISTS anime;
+CREATE TABLE `anime` (
+  `id_anime` INT NOT NULL AUTO_INCREMENT,
+  `id_contenido` INT,
+  `capitulos` INT,
+  `id_estudio` INT,
+  PRIMARY KEY (id_anime),
+  FOREIGN KEY (id_contenido) REFERENCES contenido(id_contenido)
+  FOREIGN KEY (id_estudio) REFERENCES estudioAnimacion(id_estudio)
+);
+
+--
+-- Table structure for table 'videojuego'
+--
+
+DROP TABLE IF EXISTS videojuego;
+CREATE TABLE `videojuego`(
+  `id_videojuego` INT NOT NULL AUTO_INCREMENT,
+  `id_contenido` INT,
+  `plataforma` VARCHAR(255),
+  `id_desarrollador` INT,
+  PRIMARY KEY (id_videojuego),
+  FOREIGN KEY (id_contenido) REFERENCES contenido(id_contenido)
+  FOREIGN KEY (id_desarrollador) REFERENCES id_desarrollador(id_desarrollador)
+);
+
+--
+-- Table structure for table 'director'
+--
+
+DROP TABLE IF EXISTS director;
+CREATE TABLE director (
   id_director INT NOT NULL AUTO_INCREMENT,
   nombre VARCHAR(255) NOT NULL,
   PRIMARY KEY (id_director)
 );
 
---Table structure for table 'Estudio de Animación'
+--
+-- Table structure for table 'estudioAnimacion'
+--
 
-DROP TABLE IF EXISTS Estudio_de_Animacion;
-CREATE TABLE Estudio_de_Animacion (
+DROP TABLE IF EXISTS estudioAnimacion;
+CREATE TABLE estudioAnimacion (
   id_estudio INT NOT NULL AUTO_INCREMENT,
   nombre VARCHAR(255) NOT NULL,
   PRIMARY KEY (id_estudio)
 );
 
---Table structure for table 'Productora'
+--
+-- Table structure for table 'productora'
+--
 
-DROP TABLE IF EXISTS Productora;
-CREATE TABLE Productora (
+DROP TABLE IF EXISTS productora;
+CREATE TABLE productora (
   id_productora INT NOT NULL AUTO_INCREMENT,
   nombre VARCHAR(255) NOT NULL,
   PRIMARY KEY (id_productora)
 );
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Table structure for table 'desarrollador'
+--
+DROP TABLE IF EXISTS desarrollador;
+CREATETABLE desarrollador (
+  `id_desarrollador` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255)
+  `id_videojuego` INT
+  PRIMARY KEY(id_desarrollador),
+  FOREIGN KEY(id_videojuego) REFERENCES videojuego(id_videojuego)
+);
 
--- Dump completed on 2024-06-06 19:40:39
