921ab64621ab48f164e395ab528cd90d7e1f5f43
Fernando Maximiliano Perez Elias <maxi0970@gmail.com>
2024-06-08 20:23:37 -0300

    funcion opcion perfil y opcion juegos

---
+++ Opciones/modulo_juegos.py
@@ -0,0 +1,4 @@
+#Funcion Opcion Video Juegos
+
+def opcion_juegos():
+    print("Bienvenidos a la sección de Videos Juegos.")
---
+++ Opciones/modulo_perfil.py
@@ -0,0 +1,4 @@
+#Funcion Opcion Perfil
+
+def opcion_perfil():
+    print("Bienvenido al Perfil del Usuario.")
