de05843f2c37cccfcf91f95778ff59337d3202cc
Yesica-Ibanez <yesicaesmeraldaibanez@gmail.com>
2024-06-09 19:12:40 -0300

    modulo_user_data
    Se agrega Modulo

---
+++ Código/opciones/modulo.user_data.py
@@ -0,0 +1,26 @@
+# se crea diccionario de Usuario:
+
+usuarios = {}
+
+# se crea la funcion para registrar un usuario
+
+def registrar_usuario(usuario,clave):
+
+
+    if usuario in usuarios:
+        print(f"el usuario  '{usuario}' ya se encuentra en uso.Por favor intenta con otro nombre")
+        return False
+    else:
+        usuarios[usuario] = clave
+        print(f"usuario'{usuarios}' registrado de manera exitosa")
+        return True
+    
+
+# se crea funcion para validar Usuario
+
+def validar_usuario(usuario,clave):
+    
+    if usuario in usuarios and usuarios[usuario] == clave:
+        return True
+    else:
+        return False
