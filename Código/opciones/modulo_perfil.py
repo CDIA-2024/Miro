de6723434a229b4ce1139e9949edfc47e3bf34b6
Yesica-Ibanez <yesicaesmeraldaibanez@gmail.com>
2024-06-12 20:08:00 -0300

    modulo_perfil_usuario
    Se rearma el modulo de perfil de usuario

--- Código/opciones/modulo_perfil.py
+++ Código/opciones/modulo_perfil.py
@@ -1,4 +1,30 @@
-#Funcion Opcion Perfil
+#se definie la funcion perfil de usuario
 
-def main_menu():
-    print("Bienvenido al Perfil del Usuario.")
+class Perfil_de_usuario:
+    def __init__(self, nombre, email, cumpleaños):
+        self.nombre = nombre
+        self.email = email
+        self.cumpleaños = cumpleaños
+        self.lista_favorita = []# aca podríamaos crear una tabla llamda lista favorita
+
+    def add_contenido_favorito(self, contenido_titulo):
+        self.lista_favorita.append(contenido_titulo)
+
+    def display_profile(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Correo electrónico: {self.email}")
+        print(f"Cumpleaños: {self.cumpleaños}")
+        print("Lista multimedia favorita:")
+        for contenido in self.lista_favorita:
+            print(f"- {contenido}")
+
+# Creamos un perfil de usuario
+mi_perfil = Perfil_de_usuario(nombre="Yesica", email="yesica@gmail.com", cumpleaños="06/03/1995")
+
+# Agregar contenido multimedia favorito
+mi_perfil.lista_favorita.append("TOM RAIDER")
+mi_perfil.lista_favorita.append("The Dark Knight")
+mi_perfil.lista_favorita.append("SHOGUN")
+
+# Mostrar el perfil
+mi_perfil.display_profile()
